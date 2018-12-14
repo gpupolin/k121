ff46f62b7e6837a0d8007981f7194b12edd055d4
gpupolin <gpupolin@gmail.com>
2018-12-14 17:27:16 -0200

    Change of client path for React

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port);
