35e843176d4ca9d146f21ce57260c2211264e819
gpupolin <gpupolin@gmail.com>
2018-12-14 17:10:18 -0200

    Tranfer of paths

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Amigo Secreto");
+  })
+  .listen(process.env.PORT || 3000);
