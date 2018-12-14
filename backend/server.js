83397ccbd11905ba7b8b14423551410389b64c5d
gpupolin <gpupolin@gmail.com>
2018-12-14 18:02:13 -0200

    Test using API

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,25 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+app.get("/api/friends", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "João",
+      email: "joao@gmail.com",
+      friend_id: null,
+      friend: null
+    },
+    {
+      id: 2,
+      name: "Melissa",
+      email: "melissa@gmail.com",
+      friend_id: null,
+      friend: null
+    }
+  ]);
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
